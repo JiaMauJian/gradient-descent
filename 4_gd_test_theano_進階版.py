b08a903cb8749845befad4448a3ceed6c1a68dc1
allen <amau712@gmail.com>
2016-11-03 16:10:34 +0800

    修改cost function

--- 4_gd_test_theano_進階版.py
+++ 4_gd_test_theano_進階版.py
@@ -12,50 +12,38 @@ x_data = floatX(np.array(np.linspace(-5.0, 5.0, 100)))
 y_data = floatX(np.array(x_data))
 
 ### params / init weights
-#w = theano.shared(floatX(np.random.randn(*shape[0])))
-#b = theano.shared(floatX(np.random.randn(*shape[0])))
-w = theano.shared(floatX(np.array([-1. for i in range(x_data.shape[0])])))
-b = theano.shared(floatX(np.array([-1. for i in range(x_data.shape[0])])))
-#w = theano.shared(floatX(np.random.randn(1)[0]))
-#b = theano.shared(floatX(np.random.randn(1)[0]))
-#w = theano.shared(floatX(np.array(-1.)))
-#b = theano.shared(floatX(np.array(-1.)))
+w = theano.shared(-1.)
     
 ### model
 x = T.vector()
-#x = T.scalar()
 y = w*x
 f = theano.function([x], y)
+
+### cost/error/loss
+y_hat = T.vector()
+cost = T.sum((y-y_hat)**2)
 
 ### gradients
 def gd(params, grads, lr):
     updates = []
     for p, g in it.izip(params, grads):      
-        updates.append([p, p - lr*T.sum(g)])
+        updates.append([p, p - lr*g])
     return updates
-
-def sgd(params, grads, lr):    
-    updates = []
-    for p, g in it.izip(params, grads):      
-        updates.append([p, p - lr*g])    
-    return updates
-
-### cost/error/loss
-y_hat = T.vector()
-#y_hat = T.scalar()
-cost = T.sum((y-y_hat)**2)
-#cost = (y-y_hat)**2
-
+    
 dw = T.grad(cost, w)
 train = theano.function(inputs=[x, y_hat],
                         outputs=[cost,w],
                         updates=gd([w], [dw], 0.001))
 
 cost_list = []
-for i in range(3):
+for i in range(100):    
     cost_result = train(x_data, y_data)
-    print cost_result[0], cost_result[1][0]
-    cost_list.append(cost_result[0])
+    cost = cost_result[0]
+    print "cost=%f, w=%f" % (cost, cost_result[1])
+    cost_list.append(cost)
+    
+    if float(cost) > 0 and float(cost) < 0.0000001:
+        break
     
 plt.plot(cost_list)
 plt.xlabel("No. of parameters updates")
