ff632d4901bee5ed223667cb1e7e95701d2569b8
allen <amau712@gmail.com>
2016-11-03 13:12:45 +0800

    搞清楚vector和scalar對T.grad做update的影響

--- 1_gd_test_theano.py
+++ 1_gd_test_theano.py
@@ -9,11 +9,8 @@ print floatX
 x = T.vector()
 
 #theano shared variables (global variables)
-#w = theano.shared(np.array(np.ones(2), dtype=floatX), name='w')
-#b = theano.shared(np.array(np.ones(2), dtype=floatX), name='b')
-w = theano.shared(np.array([-1. for i in range(100)], dtype=floatX), name='w')
-b = theano.shared(np.array([-1. for i in range(100)], dtype=floatX), name='b')
-
+w = theano.shared(-1., name='w')
+b = theano.shared(-1., name='b')
     
 # define function
 y = w*x**2 + b
@@ -26,18 +23,17 @@ dw, db = T.grad(cost, [w,b])
 lr = 0.00001
 gradient = theano.function(inputs=[x, y_hat],
                            outputs=[dw, db],
-                           updates=[(w, w-lr*T.sum(dw)), (b, b-lr*T.sum(db))])
+                           updates=[(w, w-lr*dw), (b, b-lr*db)])
 
 x_data = np.array(np.linspace(-5.0, 5.0, 100), dtype=floatX)
 y_data = np.array(x_data**2+1, dtype=floatX)
 
-T = 1
-t = 10000
-
-for i in range(0, T):
-    for j in range(0, t):
-        gradient(x_data, y_data)        
-    print "w=%f, b=%f" % (w.get_value()[0], b.get_value()[0])
+epochs = 10000
+    
+for j in range(0, epochs):
+    dw_sum, db_sum = gradient(x_data, y_data)        
+    #print "dw_sum=%f, db_sum=%f" % (dw_sum, db_sum)    
+print "w=%f, b=%f" % (w.get_value(), b.get_value())
     
 # define test
 cost_f = theano.function([x, y_hat], cost)
