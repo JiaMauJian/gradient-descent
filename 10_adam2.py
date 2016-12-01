612952f40108c7cd056e63d310e9db035bff1426
JiaMauJian <amau712@gmail.com>
2016-12-01 09:42:46 +0800

    add

--- 10_adam2.py
+++ 10_adam2.py
@@ -71,8 +71,8 @@ his_v_dw = []
 his_v_db = []
 def calc_adam(x, y, i):
         
-    beta1 = 0.9 # momentum
-    beta2 = 0.001 # rmsprop
+    beta1 = 0.1 # momentum
+    beta2 = 0.999 # rmsprop
     lr = 0.03
     e=1e-8    
     
@@ -124,7 +124,7 @@ def calc_adam(x, y, i):
 
 ###############################################################################
 ### adam (Alec Radford)
-def Adam2(cost, params, lr=0.03, b1=0.9, b2=0.001, e=1e-8):
+def Adam2(cost, params, lr=0.03, b1=0.1, b2=0.999, e=1e-8):
     updates = []
     grads = T.grad(cost, params)
     i = theano.shared(floatX(0.))
@@ -216,7 +216,7 @@ print '(closed-fom) w=0.0639, b= 0.7502'
 plt.plot(his_cost_by_adam.iloc[:, 0], label='adam')
 plt.plot(his_cost_by_adam2.iloc[:, 0], label='adam (Alec Radford)')
 plt.legend()
-plt.title("learning rate=0.03, beta1=0.9, beta2=0.001")
+plt.title("learning rate=0.03, beta1=0.1, beta2=0.999")
 plt.xlabel("No. of parameters updates by batch")
 plt.ylabel("Loss by batch of avg cost")
 plt.ylim([0, 0.4])
